3bb5cb7a32b49d16f5f53d0dc070bc330989fbd7
QinCalvin <calvin.qin.ca@gmail.com>
2018-05-08 16:07:37 -0700

    Refactored Loader

---
+++ src/State/StateLoader.java
@@ -0,0 +1,20 @@
+package State;
+
+public class StateLoader {
+    public State currentState;
+
+    public StateLoader(State s){
+        currentState = s;
+    }
+
+    public void load(State s){
+        currentState = s;
+    }
+
+    public void render(){
+        currentState.render();
+    }
+    public void tick(){
+        currentState.tick();
+    }
+}
