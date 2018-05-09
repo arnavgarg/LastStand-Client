15a2996c713582e1182c09b7f32d065a7dbf7eb5
kalebdawit <39032428+kalebdawit@users.noreply.github.com>
2018-05-09 14:59:44 -0700

    set up frame/panel

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,23 @@
 package PACKAGE_NAME;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
 public class Game {
+
+	public static void main(String[] args) {
+		
+		Avatar avatar = new Avatar();
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		
+		frame.setSize(1000, 750);
+		frame.setTitle("LAST STAND");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.add(panel);
+		
+	}
+	
 }
