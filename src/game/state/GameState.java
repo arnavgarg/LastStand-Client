3609f4ca49448af640b6af4bf8f6b4f19124c753
kalebdawit <39032428+kalebdawit@users.noreply.github.com>
2018-05-16 15:01:27 -0700

    Update GameState.java

--- src/game/state/GameState.java
+++ src/game/state/GameState.java
@@ -2,11 +2,43 @@ package game.state;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.awt.Image;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class GameState extends State{
 
+    Image playerBody;
+	Image playerHand;
+	private int bodyX = 350;
+	private int bodyY = 250;
+	private int rightX = 430;
+	private int rightY = 245;
+	private int leftX = 345;
+	private int leftY = 245;
+	
+	public GameState() {
+		
+		try {
+			playerBody = ImageIO.read(getClass().getResource("/res/redcircle.png"));
+			playerHand = ImageIO.read(getClass().getResource("/res/redcircle.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		playerBody = playerBody.getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+		playerHand = playerHand.getScaledInstance(20, 20, Image.SCALE_DEFAULT);
+		
+	}
+    
     public void render(Graphics g){
-        //TODO - Render the game map
+		
+        // TODO - render game map
+        g.drawImage(playerBody, bodyX, bodyY, null);
+        g.drawImage(playerHand, rightX, rightY, null);
+        g.drawImage(playerHand, leftX, leftY, null);
+        
     }
 
     public void tick(){
