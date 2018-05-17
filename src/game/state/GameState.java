0ddcb056f9d82b3c55d0e9695ca436387c7e71f5
kalebdawit <39032428+kalebdawit@users.noreply.github.com>
2018-05-16 17:34:26 -0700

    Update GameState.java

--- src/game/state/GameState.java
+++ src/game/state/GameState.java
@@ -7,25 +7,25 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class GameState extends State{
+public class GameState extends State {
 
     Image playerBody;
-	Image playerHand;
-	private int bodyX = 350;
-	private int bodyY = 250;
-	private int rightX = 430;
-	private int rightY = 245;
-	private int leftX = 345;
-	private int leftY = 245;
+    Image playerHand;
+    private int bodyX = 350;
+    private int bodyY = 250;
+    private int rightX = 430;
+    private int rightY = 245;
+    private int leftX = 345;
+    private int leftY = 245;
 	
-	public GameState() {
+    public GameState() {
 		
-		try {
-			playerBody = ImageIO.read(getClass().getResource("/res/redcircle.png"));
-			playerHand = ImageIO.read(getClass().getResource("/res/redcircle.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	try {
+		playerBody = ImageIO.read(getClass().getResource("/res/redcircle.png"));
+		playerHand = ImageIO.read(getClass().getResource("/res/redcircle.png"));
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
 		
 		playerBody = playerBody.getScaledInstance(100, 100, Image.SCALE_DEFAULT);
 		playerHand = playerHand.getScaledInstance(20, 20, Image.SCALE_DEFAULT);
