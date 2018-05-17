31c76377db7c7be7d06967d78b64753578e833e7
QinCalvin <calvin.qin.ca@gmail.com>
2018-05-17 14:38:07 -0700

    methods added to retrive data

--- src/game/map/MapInfo.java
+++ src/game/map/MapInfo.java
@@ -5,9 +5,7 @@ import java.awt.*;
 public class MapInfo {
     private Map map;
     private Point boundaryCenter;
-    private int boundaryRaduis;
-
-
+    private int boundaryRadius;
 
     private static MapInfo instance;
 
@@ -16,5 +14,22 @@ public class MapInfo {
             instance = new MapInfo();
         }
         return instance;
+    }
+
+    public void setBoundaryCenter(Point dc) {
+        boundaryCenter  = dc;
+    }
+
+    public void setBoundaryRadius(int dr) {
+        boundaryRadius = dr;
+    }
+
+
+    public Point getBoundaryCenter() {
+        return boundaryCenter;
+    }
+
+    public int getBoundaryRadius() {
+        return boundaryRadius;
     }
 }
