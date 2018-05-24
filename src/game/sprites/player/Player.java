92891d4e4e0edeb7b7025a2cf11443d8b9800123
QinCalvin <calvin.qin.ca@gmail.com>
2018-05-24 14:03:07 -0700

    draw gun of player

--- src/game/sprites/player/Player.java
+++ src/game/sprites/player/Player.java
@@ -17,7 +17,11 @@ public class Player {
     private Location loc;
     private Item[] inventory = new Item[5];
 
+    private double angle;
+    private int health; //0-100
+
     private static BufferedImage image;
+    private static BufferedImage gun;
 
     public Player(int id, String name, Location loc) {
         if (image == null) {
@@ -28,29 +32,42 @@ public class Player {
             }
         }
 
+        if (gun == null) {
+            try {
+                gun = ImageIO.read(getClass().getResource("/ClipGun.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         this.id = id;
         this.name = name;
         this.loc = loc;
+        this.angle = 1.57;
+        this.health = 100;
+    }
+
+    public void updateAngle() {
+        angle = Math.atan2(GameInfo.getInstance().getMouseY() - Game.HEIGHT/2, GameInfo.getInstance().getMouseX() -
+                Game.WIDTH/2);
+    }
+
+    private void drawGun(Graphics g, Location l) {
+        g.translate(Game.WIDTH/2 - (int) (l.diffX(this.loc)), Game.HEIGHT/2 - (int) (l.diffY(this.loc)));
+        ((Graphics2D)g).rotate(this.angle);
+        g.drawImage(gun, 10, -2, 50, 5, null);
+        ((Graphics2D)g).rotate(-this.angle);
+        g.translate(-Game.WIDTH/2, -Game.HEIGHT/2);
     }
 
     public void render(Graphics g, Location l) {
         g.drawImage(image, Game.WIDTH/2 - 25 - (int) (l.diffX(this.loc)),
                 Game.HEIGHT/2 - 25 - (int) (l.diffY(this.loc)), 50, 50, null);
+        drawGun(g, l);
         g.setColor(Color.BLACK);
         g.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
         g.drawString(name, Game.WIDTH/2 - (3*name.length()) - (int) (l.diffX(this.loc)),
                 Game.HEIGHT/2 - 27 - (int) (l.diffY(this.loc)));
-    }
-
-    public void render(Graphics g) {
-        g.drawImage(image, Game.WIDTH/2 - 25, Game.HEIGHT/2 - 25, 50, 50, null);
-        double angle = Math.atan2(GameInfo.getInstance().getMouseY() - Game.HEIGHT/2,
-                GameInfo.getInstance().getMouseX() - Game.WIDTH/2);
-        g.drawLine(Game.WIDTH/2, Game.HEIGHT/2, (int) (Game.WIDTH/2 + (50 * Math.cos(angle))),
-                (int) (Game.HEIGHT/2 + (50 * Math.sin(angle))));
-        g.setColor(Color.BLACK);
-        g.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
-        g.drawString(name, Game.WIDTH/2 - (3*name.length()), Game.HEIGHT/2 - 27);
     }
 
     public Location getLoc() {
@@ -59,6 +76,14 @@ public class Player {
 
     public int getId() {
         return id;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public int getHealth() {
+        return health;
     }
 
     @Override
